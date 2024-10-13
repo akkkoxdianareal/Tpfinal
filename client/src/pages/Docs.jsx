000d2d23c8d087073b5a5297c4b2690fd4af8d91
akkkoxdianareal <pequitas0376@gmail.com>
2024-10-13 18:40:37 -0300

    Cambios a la documentacion

--- client/src/pages/Docs.jsx
+++ client/src/pages/Docs.jsx
@@ -131,7 +131,7 @@ export default function Docs() {
                     </TabPanel>
                     {/* BackEnd */}
                     //#endregion BackEnd
-                    
+            
                     //#region Team
                     {/* Equipo */}
                     <TabPanel>
